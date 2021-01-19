a96820f7b48ca43895e8c79c8243dae9ba7cd31a
hasanmiaweb <hasanmiaweb@gmail.com>
2021-01-20 00:38:07 +0600

    first commit

---
+++ random.js
@@ -0,0 +1,9 @@
+// var randoms = Math.random() * 8;
+// var output = Math.round(randoms);
+// console.log(output);
+
+for (let i = 0; i <= 5; i++) {
+  var random = Math.random() * 5;
+  var output = Math.round(random);
+  console.log(output);
+}
---
+++ swap.js
@@ -0,0 +1,7 @@
+var a = 30;
+var b = 20;
+console.log("Before Swap" + " A:" + a, "B:" + b);
+var temp = a;
+a = b;
+b = temp;
+console.log("After Swap" + " A:" + a, "B:" + b);
